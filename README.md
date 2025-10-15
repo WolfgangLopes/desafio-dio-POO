42045bdadcf35261ec0d5c1a084129a1c324feda
Wolfgang Lopes <lwolfgang@proton.me>
2025-10-15 07:02:16 -0300

    Created classes

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Desafio DIO</h1>
+
+Desafio proposto pela DIO para aprender sobre o paradigma da programação orientada à objeto na prática.
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,9 +23,31 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(mentoria);
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Dev Camila");
+        System.out.println("Conteúdos inscritos de Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Conteúdos concluídos de Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: "+devCamila.calcularTotalXP());
+
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Dev Joao");
+        devJoao.inscreverBootamp(bootcamp);
+        devJoao.progredir();
+        System.out.println("Conteúdos inscritos de João: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de João: " + devJoao.getConteudosConcluidos());
+        System.out.println("XP: "+devJoao.calcularTotalXP());
     }
 }
